ad6d1c7b82539865fdf5bc0669504d6f32963a1c
Mohamed Abdiaziiz <iammoaziiz@gmail.com>
2020-11-10 13:50:42 +0300

    add a function for the student management system

--- student.py
+++ student.py
@@ -1,4 +1,12 @@
 import os
 import platform
+
 global listStd #Making ListStd As Super Global Variable
 listStd = ["yugesh", "kishor", "gajen", "Gopi"] #List Of Students
+
+def manageStudent(): #Function For The Student Management System
+
+	x = "#" * 30
+	y = "=" * 28
+	global bye #Making Bye As Super Global Variable
+	bye = "\n {}\n# {} #\n# ===> Brought To You By <===  #\n# ===> code-projects.org <===  #\n# {} #\n {}".format(x, y, y, x) # Will Print GoodBye
